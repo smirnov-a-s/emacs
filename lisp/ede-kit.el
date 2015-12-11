01965f7cae743be154a53148b221b8d51aeadb30
Anton Smirnov <asmirnov@stc-spb.ru>
2015-12-11 17:57:37 +0300

    ede

--- lisp/ede-kit.el
+++ lisp/ede-kit.el
@@ -1,5 +1,6 @@
 (global-ede-mode t)
 
+(when (string-match "i686-pc-linux-gnu" system-configuration)
 (ede-cpp-root-project "Signal"
                 :name "Signal Project"
                 :file "/home/anton/buildroot/home/user/repos/git_project/signal/signal/.projectile"
@@ -10,6 +11,6 @@
                 :name "Demos Project"
                 :file "/home/anton/work/my_stuff/c++/demos/.projectile"
                 :include-path '("./"
-				))
+				)))
 
 (provide 'ede-kit)
