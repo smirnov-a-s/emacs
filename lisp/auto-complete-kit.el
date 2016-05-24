ad17bedd21973cd012fe5d3743595bd3fc0aec6f
Anton Smirnov <a.fin.smirnov@gmail.com>
2016-05-25 00:01:17 +0300

    moving to rtags completion

--- lisp/auto-complete-kit.el
+++ lisp/auto-complete-kit.el
@@ -1,9 +1,12 @@
 (require 'auto-complete-config)
+(require 'rtags-ac)
 
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 ;; (ac-config-default)
 (ac-set-trigger-key "TAB")
 ;; (setq ac-auto-start nil)
+(setq ac-delay 0.1)
+(setq ac-auto-show-menu 0.5)
 (setq ac-use-quick-help nil)
 
 (require 'my-clang-cflags-kit)
