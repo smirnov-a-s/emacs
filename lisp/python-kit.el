94de870a2a3712cc43aae2572c6845ddc3ef5988
Anton Smirnov <a.fin.smirnov@gmail.com>
2016-07-25 13:07:28 +0300

    python

--- lisp/python-kit.el
+++ lisp/python-kit.el
@@ -1,5 +1,8 @@
 (defun my-python-mode-hook()
-  (add-to-list 'company-backends 'company-jedi))
+  (add-to-list 'company-backends 'company-jedi)
+  (local-set-key [f2] 'jedi:goto-definition)
+  (local-set-key (kbd "C-c <") 'jedi:goto-definition-pop-marker)
+  )
 
 (add-hook 'python-mode-hook 'my-python-mode-hook)
 
